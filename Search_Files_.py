5a7ddb81e7dc4cacff8d26a40ec8a44b5fcf005f
Mahrez BenHamad <mahrez.benhamad@gmail.com>
2018-07-25 00:08:41 +0100

    Update and rename Search_File_tool.py to Search_Files_.py

--- Search_Files_.py
+++ Search_Files_.py
@@ -1,15 +1,19 @@
 import fnmatch
 import os
 rootPath = input("Enter the path to searsh in  ")
-pattern = input("Enter the file/dic name with his extension  ")
+pattern = input("Enter the file name with his extension  ")
 print("------------------------------------------------------------------------------------")
 print("--------------------------------- Researsh Started ----------------------------------")
 print("")
 
+exist = False
 for root, dirs, files in os.walk(rootPath):
    for filename in fnmatch.filter(files, pattern):
        print( os.path.join(root, filename))
-       
+       exist = True
+if (not(exist)):
+    print(" NO FILE '{}' ".format(pattern))
+      
 print("")
 print("--------------------------------- Researsh Finished ---------------------------------")
 print("------------------------------------------------------------------------------------")
